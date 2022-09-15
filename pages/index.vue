ee7df3411e788a548753b1fe5bc871378fe09e2e
Abdel Medjaheri <medjaherirany@gmail.com>
2022-09-15 10:24:57 +0200

    fix: spacing - naming

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ const lastUpdated = computed(() => {
   return new Date(coins.value[0].updated_at).toLocaleString();
 });
 
-const updatePrice = (currentCoinIndex: number, newCoin: ICoin) => {
+const onChangePrice = (currentCoinIndex: number, newCoin: ICoin) => {
   loading.value = true;
   const isMoreOrLess = coins.value[currentCoinIndex].price > newCoin.price ? "-" : "+";
   coins.value[currentCoinIndex] = {
@@ -34,26 +34,27 @@ const updatePrice = (currentCoinIndex: number, newCoin: ICoin) => {
     color: isMoreOrLess === "+" ? "price-up-color" : "price-down-color",
     diff: parseFloat(
       Math.abs(newCoin.price - coins.value[currentCoinIndex].price).toFixed(2)
-      ),
-    };
-    setTimeout(() => {
-      coins.value[currentCoinIndex].change = null;
-      coins.value[currentCoinIndex].color = null;
-      loading.value = false;
-    }, 3000);
+    ),
+  };
+  setTimeout(() => {
+    coins.value[currentCoinIndex].change = null;
+    coins.value[currentCoinIndex].color = null;
+    coins.value[currentCoinIndex].diff = null;
+    loading.value = false;
+  }, 3000);
 };
 
 onMounted(() => {
   subscription = supabase
-  .from("coins")
-  .on("UPDATE", async (payload) => {
-    const currentCoinIndex = coins.value.findIndex(
-      (coin) => coin.id === payload.new.id
+    .from("coins")
+    .on("UPDATE", (payload) => {
+      const currentCoinIndex = coins.value.findIndex(
+        (coin) => coin.id === payload.new.id
       );
       const newCoin = payload.new;
-      
+
       if (coins.value[currentCoinIndex].price !== newCoin.price) {
-        updatePrice(currentCoinIndex, newCoin);
+        onChangePrice(currentCoinIndex, newCoin);
       }
     })
 
@@ -67,11 +68,10 @@ onUnmounted(() => {
 
 <template>
   <main className="bg-gray-800 min-h-screen flex flex-col items-center justify-center py-20">
-    <div class="flex flex-col items-center space-y-4 mb-10 px-4">
-      <button @click="onUpdate"
-        :disabled="loading"
+    <div class="flex flex-col items-center mb-10 px-4">
+      <button @click="onUpdate" :disabled="loading"
         :class="loading ? 'bg-gray-600 cursor-not-allowed' : 'bg-yellow-500 hover:bg-yellow-600'"
-        class="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
+        class="my-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
         <span>Mettre à jour les prix en base de données</span>
         <ArrowPathIcon class="h-6 w-6 ml-2" :class="loading ? 'animate-spin' : ''" />
       </button>
